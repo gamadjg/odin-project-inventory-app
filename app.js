85aa258db98c7940252d810c03a8710053e8fa54
gamadjg <gamadavid36@gmail.com>
2023-03-22 12:39:52 -0700

    Removed logger middleware. Moved all site traversal routes into their own file, main-routes. Removed unnecessary comments

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const cardRoutes = require("./routes/cards-routes");
-// const taskRoutes = require("./routes/task-routes");
-// .env is ignored, contains port and dburi for mongodb.
+const webRoutes = require("./routes/main-routes.js");
 const dburi = process.env.dbURI;
 const port = process.env.PORT;
 
@@ -19,46 +18,18 @@ mongoose
 	.then((result) => {
 		// we are only listening for requests until after the conneciton to the db is established.
 		app.listen(port);
+		console.log(`App listening on port ${port}`);
 	})
 	.catch((err) => {
 		console.log(err);
 	});
 
-// Middleware. console log method type and date for every request.
-const myLogger = function (req, res, next) {
-	console.log("Request Method: " + req.method);
-	console.log("Request date: " + new Date());
-
-	// NEED TO ADD next() to middleware that isn't returning any response.
-	next();
-};
-// Calling the middleware
-app.use(myLogger);
-
-app.get("/", (req, res) => {
-	res.render("how-to-play", { title: "How to Play" });
-});
-
-app.get("/how-to-play", (req, res) => {
-	res.render("how-to-play", { title: "How to Play" });
-});
-
-app.get("/rules", (req, res) => {
-	res.render("rules", { title: "Rules" });
-});
-
+// Setup routes to site traversal and card control
 app.use("/cards", cardRoutes);
+app.use("/", webRoutes);
 
-app.get("/card-creation", (req, res) => {
-	res.render("card-creation", { title: "Create Your Own Card!" });
-});
-
-//
-//app.use("/tasks", taskRoutes);
-
-// default 404 page
+// 404 page
 app.use((req, res) => {
-	//res.sendFile("./views/404.html", { root: __dirname });
 	res.render("404", { title: "ERROR" });
 });
 
